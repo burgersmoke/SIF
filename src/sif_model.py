39bc558add92b79945c0e798b0d302f99cd335d7
burgersmoke <burgersmoke@gmail.com>
2019-06-15 18:05:44 -0600

    Simplified how models are loaded by using stored member values of necessary pieces like file path, embeddings format, lowercasing behavior, etc so that a simple helper can be used for loading embeddings and then the rest of the model.

--- src/sif_model.py
+++ src/sif_model.py
@@ -9,6 +9,9 @@ from sklearn.externals import joblib as sklearn_joblib
 import data_io, params, SIF_embedding
 from SIF_embedding import get_weighted_average
 
+# helper for word2vec format 
+from data_io_w2v import load_w2v_word_map
+
 import numpy as np
 
 # This class serves as a means of fitting an SIF model and then being able to transform other sentence vectors later
@@ -19,6 +22,10 @@ class SIFModel(object):
         self.svd = None
         self.word_map = None
         self.params = params
+        self.sentence_count = -1
+        self.lowercase_tokens = False
+        self.embeddings_filepath = None
+        self.embeddings_format = None
 
     def transform(self, We, sentences):
         x, m = data_io.sentences2idx(sentences, self.word_map) # x is the array of word indices, m is the binary mask indicating whether there is a word in that location
@@ -42,12 +49,17 @@ class SIFModel(object):
             
         return XX
         
-    def fit(self, sentences, We, params, word_map, weight4ind):
+    def fit(self, sentences, We, lowercase_tokens, embeddings_format, embeddings_filepath, params, word_map, weight4ind):
         
         # store these off for pickling or extra transforms
         self.word_map = word_map
         self.weight4ind = weight4ind
         self.params = params
+        self.lowercase_tokens = lowercase_tokens
+        self.embeddings_format = embeddings_format
+        self.embeddings_filepath = embeddings_filepath
+        
+        self.sentence_count = len(sentences)
         
         x, m = data_io.sentences2idx(sentences, self.word_map) # x is the array of word indices, m is the binary mask indicating whether there is a word in that location
         w = data_io.seq2weight(x, m, self.weight4ind) # get word weights
@@ -61,3 +73,19 @@ class SIFModel(object):
         self.trained = True
         
         return self.remove_pc(weighted_emb)
+        
+    @staticmethod
+    def embedding_loading_helper(embeddings_filepath, embeddings_format):
+        words = None
+        We = None
+        if embeddings_format == 'GLOVE':
+            print('Loading embeddings as GLOVE')
+            (words, We) = data_io.load_glove_word_map(embeddings_filepath)
+        elif embeddings_format == 'WORD2VEC_BIN':
+            (words, We) = load_w2v_word_map(embeddings_filepath, binary = True)
+        elif embeddings_format == 'WORD2VEC_TXT':
+            (words, We) = load_w2v_word_map(embeddings_filepath, binary = False)
+        else:
+            print('Unknown embeddings format : {}'.format(embeddings_format))
+            
+        return words, We
