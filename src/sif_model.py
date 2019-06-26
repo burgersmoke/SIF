ae124497805bc0f98ffb2df7bf15f0e45b02f292
burgersmoke <burgersmoke@gmail.com>
2019-06-25 21:41:01 -0600

    Updated an optional method to attempt to perform backoff of various token casings in attempts to find alternate lexical variations in a weight lookup.  Also adding an alternate weighted averaged function which avoids divide by zero.

--- src/sif_model.py
+++ src/sif_model.py
@@ -14,6 +14,76 @@ from data_io_w2v import load_w2v_word_map
 
 import numpy as np
 
+from past.builtins import xrange
+
+# this is a modified version of getWeight() from data_io.py
+# the main difference is that there is an option here to look up word keys as
+# different casings since the default 
+def getWeightAlternate(words, word2weight, 
+        attempt_other_cases = True,
+        report_oov = True):
+    weight4ind = {}
+    
+    word_oov_set = set()
+    word_remapping_set = set()
+    
+    for word, ind in words.items():
+    
+        word_key = word
+        if attempt_other_cases:
+            if word_key not in word2weight:
+                word_lower = word.lower()
+                word_capital = word.capitalize()
+                word_upper = word.upper()
+                
+                # let's try these in order
+                if word_lower in word2weight:
+                    word_key = word_lower
+                elif word_capital in word2weight:
+                    word_key = word_capital
+                elif word_upper in word2weight:
+                    word_key = word_upper
+    
+        if word_key in word2weight:
+            weight4ind[ind] = word2weight[word_key]
+            
+            if word != word_key:
+                word_remapping_set.add(word)
+            
+        else:
+            weight4ind[ind] = 1.0
+            word_oov_set.add(word)
+            
+    if report_oov:
+    
+        print('Total words remapped : {}'.format(len(word_remapping_set)))
+        print('Total word-weight OOV : {}'.format(len(word_oov_set)))
+    
+        #word_oov_sorted = sorted(list(word_oov_set))
+        #print('Out of vocabulary with respect to word weighting:')
+        #print(word_oov_sorted)
+        
+    return weight4ind
+
+def get_weighted_average_alternate(We, x, w):
+    """
+    Compute the weighted average vectors
+    :param We: We[i,:] is the vector for word i
+    :param x: x[i, :] are the indices of the words in sentence i
+    :param w: w[i, :] are the weights for the words in sentence i
+    :return: emb[i, :] are the weighted average vector for sentence i
+    """
+    n_samples = x.shape[0]
+    emb = np.zeros((n_samples, We.shape[1]))
+    for i in xrange(n_samples):
+        denom = np.count_nonzero(w[i,:])
+        
+        if denom <= 0.0:
+            print('WHOA!  Sample [{0}] attempted to compute a denominator of : [{1}]'.format(i, denom))
+        else:
+            emb[i,:] = w[i,:].dot(We[x[i,:],:]) / denom
+    return emb
+
 # This class serves as a means of fitting an SIF model and then being able to transform other sentence vectors later
 # This also allows save/loading model components via scikit-learn's joblib implementation
 class SIFModel(object):
@@ -66,7 +136,8 @@ class SIFModel(object):
         
         # now let's do some of what happens in src/SIF_embedding.py
         # but also keep some pieces along the way
-        weighted_emb = get_weighted_average(We, x, w)
+        #weighted_emb = get_weighted_average(We, x, w)
+        weighted_emb = get_weighted_average_alternate(We, x, w)
         
         self.compute_pc(weighted_emb)
         
